4a3459549b31edb27e517a2d2adccddbb247c3f7
Mr Bazlow <oliver.mckee@gmail.com>
2023-01-10 21:10:35 +0000

    shards changes

--- src/foxholeShards.js
+++ src/foxholeShards.js
@@ -1,14 +1,13 @@
+/**
+ * @module src/foxholeShards
+ */
+
+/**
+ * @type {object.<object>}
+ */
 exports.shardData = {
 	able: {
 		name: 'ABLE',
 		api: 'https://war-service-live.foxholeservices.com/api/',
-	},
-	baker: {
-		name: 'BAKER',
-		api: 'https://war-service-live-2.foxholeservices.com/api/',
-	},
-	charlie: {
-		name: 'CHARLIE',
-		api: 'https://war-service-live-3.foxholeservices.com/api/',
 	},
 };
